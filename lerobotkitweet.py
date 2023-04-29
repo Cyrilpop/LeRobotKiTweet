3da98e1f7c65a29b22e222ac22e2d3a3859bbc0b
Cyrilpop <75445832+Cyrilpop@users.noreply.github.com>
2023-04-29 23:57:35 +0200

    Refactorisation

--- lerobotkitweet.py
+++ lerobotkitweet.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python3
-import os
-import sys
-import time
-import random
 import logging
+import os
+import random
 import requests
+import sys
+import datetime
+from typing import List
+
 import json
+import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+from fuzzywuzzy import fuzz
 from GoogleNews import GoogleNews
 from newspaper import Article
-import tweepy
 import pyshorteners
+import tweepy
+from unidecode import unidecode
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+import feedparser
+import yaml
+
+
 
 # Création du répertoire de log s'il n'existe pas
 log_dir = "/var/log/twittos"
-log_titles = set()
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
-
-# Récupération du sujet
-if len(sys.argv) < 2:
-    print("Usage: robotKiTweet.py [sujet]")
-    sys.exit(1)
-else:
-    sujet = " ".join(sys.argv[1:])
 
 # Configuration du logging
 logging.basicConfig(
@@ -30,37 +35,69 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(message)s",
 )
-
+published_articles = set()
+script_name = os.path.basename(__file__)
+logging.info("{:=^90}".format(" {} ".format(script_name)))
+# Chargement du fichier de conf
+logging.info('Chargement du fichier de configuration')
+current_dir = os.path.dirname(os.path.abspath(__file__))
+with open(f'{current_dir}/lerobotkitweet.yml', 'r') as stream:
+     config = yaml.safe_load(stream)
 # Récupération des titres déjà traités
+logging.info('Récupération des titres déjà traités')
 with open(f"{log_dir}/Twitos.log", "r") as f:
     for line in f:
         if "Titre article:" in line:
             log_title = line.split("Titre article: ")[1].strip()
-            log_titles.add(log_title)
+            published_articles.add(log_title)
 
+published_articles = set()
+google_trend_rss = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=FR&hl=fr'
 
-def launch_conditions():
+def get_google_trends(url):
+    logging.info(f'Lancement de la fonction get_google_trends avec le paramètre {url}')
+    # Charger le flux RSS
+    feed = feedparser.parse(url)
+    # Initialiser la liste des résultats
+    results = []
+    # Parcourir tous les items du flux RSS
+    for item in feed.entries:
+        # Récupérer les informations nécessaires pour chaque item
+        title = item.title
+        logging.info(f'Titre article: {title}')
+        approx_traffic = item.get("ht_approx_traffic", "")
+        description = item.description
+        link = item.link
+        # Ajouter les informations à la liste des résultats
+        results.append({
+            "titre": title,
+            "trafic_approximatif": approx_traffic,
+            "description": description,
+            "lien": link
+        })
+        return results[0]['titre']
+
+def launch_conditions(subject):
+    logging.info(f'Lancement de la fonction launch_conditions avec le paramètre {subject}')
     if os.getenv("SHELL") == "/bin/sh":
-        logging.info(f"Le script est lancé par cron avec les paramètre {sujet}.")
-        nombre_aleatoire = random.randint(0, 8)
-        duree_en_secondes = random.randint(45, 480)
-        time.sleep(duree_en_secondes)
+        logging.info(f"Le script est lancé par cron avec les paramètre {subject}.")
     else:
-        logging.info(f"Le script est lancé manuellement avec les paramètre {sujet}.")
-
+        logging.info(f"Le script est lancé manuellement avec les paramètre {subject}.")
 
 def tweepy_client():
+    logging.info(f'Lancement de la fonction tweepy_client')
+    logging.info('Set des varoables pour l API tweepy')
     return tweepy.Client(
-        consumer_key="",
-        consumer_secret="",
-        access_token="",
-        access_token_secret="",
+        consumer_key = config['tweeter_keys']['consumer_key'],
+        consumer_secret = config['tweeter_keys']['consumer_secret'],
+        access_token = config['tweeter_keys']['access_token'],
+        access_token_secret = config['tweeter_keys']['access_token_secret']
     )
 
-
-def get_gpt_response(prompt: str):
+def get_gpt_response(prompt: str, temperature: float = 0.8):
     try:
-        OPENAI_API_KEY = ""
+        logging.info(f'Lancement de la fonction get_gpt_response avec les paramètre prompt et temerature: {temperature}')
+        OPENAI_API_KEY = config['openai_key']
         url = "https://api.openai.com/v1/chat/completions"
         headers = {
             "Content-Type": "application/json",
@@ -71,26 +108,30 @@ def get_gpt_response(prompt: str):
             "messages": [
                 {
                     "role": "sytem",
-                    "content": "Tu es expert en SEO Twiter. Tes réponses ne dépassent pas 260 cractères. Soient environs 30 mots",
+                    "content": "Tu es influenceur Tweeter. Tu matrises le SEO. Tes réponses doivent faire 3 phrases, 250 caractères",
                     "role": "user",
                     "content": prompt,
                 }
             ],
-            "temperature": 0.7,
+            "temperature": temperature,
+            "max_tokens": 60,
         }
         logging.info("Appel de l API GPT")
         response = requests.post(url, headers=headers, data=json.dumps(data))
         response_data = json.loads(response.content)
+        logging.info(f"Nombre de token utilisés : {response_data['usage']['total_tokens']}")
         return response_data["choices"][0]["message"]["content"]
     except Exception as e:
         logging.error(f"Erreur lors de l'appel à l'API OpenAI : {e}")
         return None
 
-
-def publish_tweet(client, tweet_content: str, url_source: str = None):
+def publish_tweet(client, tweet_content: str, short_url: str = None):
     try:
-        if url_source is not None:
-            tweet_content += f" {url_source}"
+        logging.info(f'Lancement de la fonction publish_tweet avec le paramètre tweet_content:  {tweet_content} {short_url}')
+        if short_url is not None:
+            tweet_content += f" {short_url}"
+        tweet_c = tweet_content.replace('"', '')
+        tweet_content = tweet_c
         logging.info(f"Publication du Tweet {tweet_content}")
         response = client.create_tweet(text=tweet_content)
         return response
@@ -98,87 +139,143 @@ def publish_tweet(client, tweet_content: str, url_source: str = None):
         logging.error(f"Erreur lors de la publication du Tweet {tweet_content}: {e}")
         return None
 
+def article_is_published(articles_data_published: List[str], article_to_compare: str, tolerance: int = 50) -> bool:
+    logging.info(f'Lancement de la fonction article_published')
+    lemmatizer = WordNetLemmatizer()
+    tokens_to_compare = word_tokenize(article_to_compare)
+    lemmas_to_compare = [lemmatizer.lemmatize(token) for token in tokens_to_compare]
+    for article_published in articles_data_published:
+        tokens_published = word_tokenize(article_published)
+        lemmas_published = [lemmatizer.lemmatize(token) for token in tokens_published]
+        ratio = fuzz.ratio(' '.join(tokens_published), ' '.join(tokens_to_compare))
+        if ratio >= tolerance:
+            logging.info(f'Le ratio vaut : {ratio}, l\'article a déjà été publié')
+            return True
+        else:
+            logging.info(f'Le ratio vaut : {ratio}, l\'article n\'a pas été publié, on le publie')
+            return False
+
+def get_articles(google_news, excluded_terms=None):
+    logging.info(f'Lancement de la fonction get_articles')
+    s = pyshorteners.Shortener()
+    if excluded_terms is None:
+        excluded_terms = []
+    if len(google_news) > 0:
+        for article in google_news[1:50]:
+            titre = article["title"]
+            logging.info(f'Titre article: {titre}')
+            if not safe_search(titre):
+                continue
+            if article_is_published(published_articles, titre):
+                continue
+            else:
+                logging.info(f'Larticle {titre} a déjà été publié.')
+            short_url = s.tinyurl.short(article["link"])
+            article_url = article["link"]
+            if not safe_search(article_url):
+                continue
+            article = Article(article_url)
+            article.download()
+            article.html
+            article.parse()
+            article_content = article.text
+            formated_content = unidecode(article_content.replace(" ", "-").lower())
+            if not safe_search(formated_content):
+                continue
+            return article_content, short_url
+        return None
+    else:
+        logging.warning(f"Aucun résultat n'a été trouvé pour la recherche")
+        return None
+
+def get_subject():
+    logging.info(f'Lancement de la fonction get_subject')
+    subjects = config['subjects']
+    poids = [subjects[key]['weight'] for key in subjects]
+    chosen_subject = random.choices(list(subjects.keys()), weights=poids, k=1)[0]
+    return subjects[chosen_subject]['name']
+
+def get_prompt(article_content: str = None, subject: str = None):
+    logging.info(f'Lancement de la fonction get_subject avec les paramètres article_content et subject : {subject}')
+    nb_days = (datetime.datetime.now() - datetime.datetime(2023, 4, 23)).days
+    if subject == 'humeur_matin':
+        prompt = "Ecris un tweet 3 phrases courtes, avec des émoticones. Tu dois donner la bonne humeur, le smile et la motivation pour cette journée. Ton tweet doit OBLIGATOIREMENT commencer par #SalutLesTerriens et se termier par 3 ou 4 hashtags. Il doit se terminer par le hashtag LeRobotKiTweet"
+    elif subject == 'humeur_soir':
+        prompt = f"Tu es LeRobotKiTweet, un petit robot intergalactique qui est arrivé dans notre monde depuis {nb_days} jours. C' est la fin de la journéer. Ecris un twwet de trois phrases courtes où tu  donnes ton point de vue, tes sentiments, tu as le droit d'être nostalgique, mais ton message doit être positif. Ecris un tweet de 3 phrases courtes avec des émoticonesi pour terminer en beauté cette journée. Ton tweet doit OBLIGATOIREMENT commencer par #BonneNuitLesTerriens et se termier par 3 ou 4 hashtags le derbier doit etre  LeRobotKiTweet. IL NE DOIT PAS CONTENIR PLUS DE 250 CARACTERES"
+    elif subject == 'etienne_klein':
+        prompt = f"Ecris un tweet dans le style de Etienne Klein de 3 phrases résumant l'article suivant. Il doit contenir des emoticones et des hashtags. Ajoute après les hashtags @EtienneKlein  : {article_content}"
+    elif subject == 'Too_long':
+        prompt = f"Le tweet suivant est trop long, raccourcis le en gardabs des hashtags ainsi que les emoticones, je serai déçu s'il dépassait 250 caractères : {article_content}"
+    else:
+        prompt = f"Ecris un tweet en français de 3 phrases, avec les émoticones appropriées. Le texte doit être très court. Le tweet doit être aguicheur, je serai deçu s'il dépassait 250 caractères, contenir 3 ou 4 hashtag et traiter le subject suivant : {article_content}"
+    return prompt
+
+def get_google_news(subject: str):
+    logging.info(f'Lancement de la fonction get_google_news avec le paramètre subject : {subject}')
+    googlenews = GoogleNews(lang="en", region="com")
+    googlenews.search(subject)
+    google_news = googlenews.result()
+    return google_news
+
+def check_subject(subject: str):
+    logging.info(f'Lancement de la fonction check_subject avec le paramètre subject : {subject}')
+    subjects_list = ['etienne_klein','humeur_matin','humeur_soir','google_trends']
+    if subject not in subjects_list:
+        print('subject not autorized')
+        sys.exit(1)
+
+def safe_search(search: str):
+    excluded_terms = config['excluded_terms']
+    logging.info(f'Lancement de la fonction safe_search avec le paramètre search : {search}')
+    excluded_terms_check = [unidecode(term.replace(" ", "-").lower()) for term in excluded_terms]
+    search_safer = unidecode(search.replace(" ", "-").lower())
+    if any(term in search_safer for term in excluded_terms_check):
+        return False
+    else:
+        return True
 
 def main():
     try:
-        excluded_terms = ["rassemblement nationnal", "algerie"]
-        excluded_words = " ".join(excluded_terms)
-        launch_conditions()
-        client = tweepy_client()
-
-        if sujet in ["humeur_matin", "humeur_soir"]:
-            prompt = "Tu joues le role de LeRobotKiTweet, un petit robot intergalactique qui est arrivé dans notre monde. C'est {0}, {1} Le texte est pour twitter, il doit être {2}, ton message doit être unique et original, donner la bonne humeur, le smile et faire environ {3} mots. Il doit se terminer par 3 ou 4 hashtags donnt le premier doit etre LeRobotKiTweet.".format(
-                "le début de journée"
-                if sujet == "humeur_matin"
-                else "la fin de journée",
-                "et tu donnes ton humeur du jour."
-                if sujet == "humeur_matin"
-                else "tu racontes toutes les aventures que tu as faites durant celle-ci. Tu donnes ton point de vue, tes sentiments. Parfois tu as le droit d'être triste mais ton message doit être positif.",
-                "drole" if sujet == "humeur_matin" else "positif",
-                "25" if sujet == "humeur_matin" else "30",
-            )
-            tweet_content = get_gpt_response(prompt)
-            if tweet_content is not None:
-                publish_tweet(client, tweet_content)
+        short_url = None
+        if len(sys.argv) < 2:
+            subject = get_subject()
+            search_activated = True
         else:
-            s = pyshorteners.Shortener()
-            googlenews = GoogleNews(lang="fr", region="FR")
-            excluded_terms = ["rassemblement nationnal", "bardela", "le pen"]
-            logging.info("Appel de l API GoogleNew")
-            googlenews.search(f"{sujet} -'rassemblement nationnal' -'algerie'")
-            result = googlenews.result()
-            if len(result) > 0:
-                for article in result[1:10]:
-                    titre = article["title"]
-                    # Vérifier si le titre contient des termes interdits et passer à l'itération suivante si c'est le cas
-                    if any(term in titre.lower() for term in excluded_terms):
-                        logging.info(
-                            f"Un des termes parmi {excluded_terms} a été trouvé dans le titre !"
-                        )
-                        continue
-                    if titre in log_titles:
-                        logging.info(
-                            f"L'article '{titre}' a déjà été traité. Passage à l'article suivant."
-                        )
-                        continue
-                    url_source = s.tinyurl.short(article["link"])
-                    article = Article(url_source)
-                    article.download()
-                    article.html
-                    article.parse()
-                    contenu = article.text
-                    if any(term in titre.lower() for term in excluded_terms):
-                        logging.info(
-                            f"Un des termes parmi {excluded_terms} a été trouvé dans le contenu !"
-                        )
-                        continue
-                    prompt = f"Résume le input input en un court texte de 20 mots maximum et 250 caractères maximum également. Le texte de retour doit être écrit en francais et compter 25 mots MAXIMUM. Il doit finir par 3 ou 4 hashtag (sans tiret ni apostrophe) racoleurs, et donnant envie et vendeurs. Input : {contenu}"
-                    logging.info(f"Titre article: {titre}")
-                    tweet_content = get_gpt_response(prompt)
-                    if tweet_content is not None:
-                        logging.info(
-                            f"Publication du tweet: {tweet_content} {url_source}"
-                        )
-                        if len(tweet_content) < 260:
-                            publish_tweet(client, tweet_content, url_source)
-                        else:
-                            prompt = f"Le texte en imput est trop long, fais un résumé de 20 mots, soient 250 caractères maiximum. Conserve les hashtags. Le texte est le suivant : {tweet_content}"
-                            tweet_content = get_gpt_response(prompt)
-                            if len(tweet_content) < 260:
-                                publish_tweet(client, tweet_content, url_source)
-                            else:
-                                logging.warning(
-                                    f"Impossible de tweeter, malgré deux résumé, le tweet est toujours trop long"
-                                )
-                    break
+            subject = " ".join(sys.argv[1:])
+            check_subject(subject)
+            search_activated = False
+            if subject == 'etienne_klein':
+                sujbect = 'etienne +klein'
+                search_activated = True
+            elif subject == 'google_trends':
+                subject = get_google_trends(google_trend_rss)
+                search_activated = True
+        launch_conditions(subject)
+        if safe_search(subject):
+            if search_activated:
+                google_news = get_google_news(subject)
+                article_content, short_url = get_articles(google_news)
             else:
-                logging.warning(
-                    f"Aucun résultat n'a été trouvé pour la recherche {sujet}."
-                )
+                article_content = None
+        else:
+            sys.exit()
+        prompt = get_prompt(article_content, subject)
+        client = tweepy_client()
+        tweet = get_gpt_response(prompt)
+        max_tweet_length = 260
+        max_attempts = 3
+        attempts = 0
+        while len(tweet) > max_tweet_length and attempts < max_attempts:
+            logging.warning(f"Impossible de tweeter : {tweet} (longueur : {len(tweet)})")
+            get_prompt(tweet, 'too_long')
+            attempts += 1
+        if len(tweet) > max_tweet_length:
+            logging.warning(f"Impossible de tweeter : {tweet} (longueur : {len(tweet)})")
+        else:
+            publish_tweet(client, tweet, short_url)
     except Exception as e:
-        logging.error(f"Une erreur s'est produite : {e}")
+        logging.error(f"Une erreur critique s'est produite : {e}")
         pass
-
 
 if __name__ == "__main__":
     main()
