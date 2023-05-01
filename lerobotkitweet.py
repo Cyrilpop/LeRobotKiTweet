1139c7870e0809d6409eb25a5ee3748de56adecc
Cyrilpop <75445832+Cyrilpop@users.noreply.github.com>
2023-05-01 17:39:21 +0200

    Refacto function article_is_publushed

--- lerobotkitweet.py
+++ lerobotkitweet.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 # Imports du module standard Python
+import json
 import logging
 import os
 import random
 import sys
-from typing import List
+
+from difflib import SequenceMatcher
+import feedparser
+import nltk
+import pyshorteners
 
 # Imports de modules externes
 import requests
-import json
+import tweepy
 import yaml
-import nltk
+from GoogleNews import GoogleNews
+from newspaper import Article
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from unidecode import unidecode
-from fuzzywuzzy import fuzz
-from GoogleNews import GoogleNews
-from newspaper import Article
-import pyshorteners
-import tweepy
-import feedparser
 
 # Téléchargements de données pour nltk
 nltk.download('punkt', quiet=True)
@@ -36,7 +36,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(message)s",
 )
-published_articles = set()
+
 script_name = os.path.basename(__file__)
 logging.info("{:=^90}".format(" {} ".format(script_name)))
 
@@ -46,29 +46,33 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 with open(f'{current_dir}/application.yml', 'r') as stream:
     config = yaml.safe_load(stream)
 
-# Récupération des titres déjà traités
-logging.info('Récupération des titres déjà traités')
-
-published_articles = set()
 google_trend_rss = 'https://trends.google.com/trends/trendingsearches/daily/rss?geo=FR&hl=fr'
 
 
-def article_is_published(articles_data_published: List[str], article_to_compare: str, tolerance: int = 50) -> bool:
-    logging.info(f'Lancement de la fonction article_is_published')
+def article_is_published(article_to_compare: str, tolerance: int = 50, comparison_type: str = 'title') -> bool:
+    """
+    Vérifie si un article a déjà été publié en comparant le texte fourni avec les articles publiés dans articles.json.
+    :param articles_data_published: Chemin vers le fichier JSON contenant les données des articles déjà publiés.
+    :param article_to_compare: Texte à comparer avec les articles déjà publiés.
+    :param tolerance: Taux de ressemblance minimum pour considérer qu'un article a été publié.
+    :param comparison_type: Type de comparaison à effectuer ('title', 'url' ou 'content').
+    :return: True si l'article a été publié, False sinon.
+    """
+    logging.info(f'Lancement de la fonction article_is_published pour {comparison_type}')
+    with open(f'{current_dir}/articles.json', 'r') as f:
+        data = json.load(f)
+
     lemmatizer = WordNetLemmatizer()
     tokens_to_compare = word_tokenize(article_to_compare)
     lemmas_to_compare = [lemmatizer.lemmatize(token) for token in tokens_to_compare]
-    i = 0
-    for article_published in articles_data_published:
-        tokens_published = word_tokenize(article_published)
+
+    for article in data['articles']:
+        text_published = article[comparison_type]
+        tokens_published = word_tokenize(text_published)
         lemmas_published = [lemmatizer.lemmatize(token) for token in tokens_published]
-        ratio = fuzz.ratio(' '.join(tokens_published), ' '.join(tokens_to_compare))
-        i += 1
-        logging.debug(f'Ratio ID #{i}: {ratio}')
+        ratio = SequenceMatcher(None, ' '.join(tokens_published), ' '.join(tokens_to_compare)).ratio()
         if ratio >= tolerance:
-            logging.info(f'Le ratio vaut : {ratio}, l\'article a déjà été publié')
             return True
-    logging.info(f'Le ratio vaut : {ratio}, l\'article n\'a pas été publié, on le publie')
     return False
 
 
@@ -87,22 +91,20 @@ def get_articles(google_news, excluded_terms=None):
         excluded_terms = []
     if len(google_news) > 0:
         for article in google_news[1:50]:
-            titre = article["title"]
-            if not safe_search(titre, 'title'):
+            article_title = article["title"]
+            if not safe_search(article_title, 'title'):
                 print('Title not safe')
-                logging.info(f'L\'article {titre} a été rejeté')
+                logging.info(f'L\'article {article_title} a été rejeté')
                 continue
-            published_articles = get_published_articles()
-            published_articles = list(published_articles)
-            if article_is_published(published_articles, titre):
-                logging.info(f'L article {titre} a déjà été publié.')
+            if article_is_published(article_title, 50, 'title'):
+                logging.info(f'L article {article_title} a déjà été publié.')
                 continue
             short_url = s.tinyurl.short(article["link"])
             article_url = article["link"]
             if not safe_search(article_url, 'url'):
                 logging.warning(f'L\'url contient des mots indterdits : {article_url}')
                 continue
-            logging.info(f'Titre article: {titre}')
+            logging.info(f'Titre article: {article_title}')
             logging.info(f"Date parution article : {article['date']}")
             logging.info(f"Url article {article_url}")
             article = Article(article_url)
@@ -113,13 +115,16 @@ def get_articles(google_news, excluded_terms=None):
                 continue
             article.html
             article.parse()
-            article_content = article.text
+            article_content = article.text.encode().decode('unicode_escape')
+            if article_is_published(article_content, 20, 'content'):
+                logging.warning(f'L\'article {article_title} a déjà até publié')
+                continue
             article_date = article.publish_date
             formated_content = unidecode(article_content.replace(" ", "-").lower())
             if not safe_search(formated_content, 'article_content'):
                 logging.info('Le contenu de l\'article contient des termes indterdits')
                 continue
-            return article_content, short_url
+            return article_title, short_url, article_url, article_date, article_content
         return None
     else:
         logging.warning(f"Aucun résultat n'a été trouvé pour la recherche")
@@ -160,7 +165,8 @@ def get_google_trends(url):
 
 def get_gpt_response(prompt: str, temperature: float = 0.8):
     try:
-        logging.info(f'Lancement de la fonction get_gpt_response avec les paramètre prompt et temerature: {temperature}')
+        logging.info(
+            f'Lancement de la fonction get_gpt_response avec les paramètre prompt et temerature: {temperature}')
         OPENAI_API_KEY = config['chat-GPT']['openai_key']
         url = "https://api.openai.com/v1/chat/completions"
         headers = {
@@ -205,13 +211,13 @@ def get_prompt(article_content: str = None, subject: str = None):
     return prompt
 
 
-def get_published_articles():
+def get_published_article_titles():
     with open(f"{log_dir}/Twitos.log", "r") as f:
         for line in f:
             if "Titre article:" in line:
                 log_title = line.split("Titre article: ")[1].strip()
-                published_articles.add(log_title)
-    return published_articles
+                published_article_titles.add(log_title)
+    return published_article_titles
 
 
 def get_subject():
@@ -248,6 +254,20 @@ def publish_tweet(client, tweet_content: str, short_url: str = None):
         return None
 
 
+def push_article_json(article_title: str, article_url: str, article_content: str):
+    logging.info(f'Lancement de la fonction push_article_json')
+    with open(f'{current_dir}/articles.json', 'r') as f:
+        articles = json.load(f)
+    new_article = {
+        "title": article_title,
+        "url": article_url,
+        "content": article_content
+    }
+    articles["articles"].append(new_article)
+    with open(f'{current_dir}/articles.json', 'w') as f:
+        json.dump(articles, f, indent=4)
+
+
 def safe_search(search: str, type_search: str = ''):
     excluded_terms = config['excluded_terms']
     logging.info(f'Lancement de la fonction safe_search avec le paramètre {type_search}')
@@ -263,10 +283,10 @@ def tweepy_client():
     logging.info(f'Lancement de la fonction tweepy_client')
     logging.info('Set des varoables pour l API tweepy')
     return tweepy.Client(
-        consumer_key = config['tweeter_keys']['consumer_key'],
-        consumer_secret = config['tweeter_keys']['consumer_secret'],
-        access_token = config['tweeter_keys']['access_token'],
-        access_token_secret = config['tweeter_keys']['access_token_secret']
+        consumer_key=config['tweeter_keys']['consumer_key'],
+        consumer_secret=config['tweeter_keys']['consumer_secret'],
+        access_token=config['tweeter_keys']['access_token'],
+        access_token_secret=config['tweeter_keys']['access_token_secret']
     )
 
 
@@ -292,11 +312,12 @@ def main():
         if safe_search(subject, 'subject'):
             if search_activated:
                 google_news = get_google_news(subject, lang)
-                article_content, short_url = get_articles(google_news)
+                article_title, short_url, article_url, article_date, article_content = get_articles(google_news)
             else:
                 article_content = None
         else:
             sys.exit()
+        push_article_json(article_title, article_url, article_content)
         prompt = get_prompt(article_content, subject)
         client = tweepy_client()
         tweet = get_gpt_response(prompt)
@@ -306,6 +327,7 @@ def main():
         while len(tweet) > max_tweet_length and attempts < max_attempts:
             logging.warning(f"Impossible de tweeter : {tweet} (longueur : {len(tweet)})")
             prompt = get_prompt(tweet, 'too_long')
+            logging.warning(f"Le nouveau prompt est {prompt}")
             tweet = get_gpt_response(prompt)
             attempts += 1
         if len(tweet) > max_tweet_length:
